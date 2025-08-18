3eae1a46258471002d81390edb3f07c608ac052e
Amit Meel <amit.amitmeel.meel@gmail.com>
2025-08-18 20:23:48 +0530

    added graph for the complete process from creating analysts to generate the final report

--- module_4/assistant.py
+++ module_4/assistant.py
@@ -523,3 +523,26 @@ def finalize_report(state: ResearchGraphState):
         final_report += "\n\n## Sources\n" + sources
     return {"final_report": final_report}
 
+# Add nodes and edges 
+builder = StateGraph(ResearchGraphState)
+builder.add_node("create_analysts", create_analysts)
+builder.add_node("human_feedback", human_feedback)
+builder.add_node("conduct_interview", interview_builder.compile())
+builder.add_node("write_report",write_report)
+builder.add_node("write_introduction",write_introduction)
+builder.add_node("write_conclusion",write_conclusion)
+builder.add_node("finalize_report",finalize_report)
+
+# Logic
+builder.add_edge(START, "create_analysts")
+builder.add_edge("create_analysts", "human_feedback")
+builder.add_conditional_edges("human_feedback", initiate_all_interviews, ["create_analysts", "conduct_interview"])
+builder.add_edge("conduct_interview", "write_report")
+builder.add_edge("conduct_interview", "write_introduction")
+builder.add_edge("conduct_interview", "write_conclusion")
+builder.add_edge(["write_conclusion", "write_report", "write_introduction"], "finalize_report")
+builder.add_edge("finalize_report", END)
+
+# Compile
+memory = MemorySaver()
+graph = builder.compile(interrupt_before=['human_feedback'], checkpointer=memory)
